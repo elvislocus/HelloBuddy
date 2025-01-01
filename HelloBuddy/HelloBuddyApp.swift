081fa552aec888fd765eabf49533757869e8dc73
Elvis Lin <elvislocus@gmail.com>
2025-01-01 22:33:10 +0900

    Create HelloBuddy project

---
+++ HelloBuddy/HelloBuddyApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelloBuddyApp.swift
+//  HelloBuddy
+//
+//  Created by Elvis Lin on 2025/1/1.
+//
+
+import SwiftUI
+
+@main
+struct HelloBuddyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
